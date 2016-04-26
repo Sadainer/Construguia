c59eb786319de2c9384e5cddba2a4f6118f5d691
sadainer <sadainer@hotmail.com>
2016-04-26 16:12:57 -0500

    arrayadpater

--- app/src/main/java/com/construguia/tecnoparque/construguia/ConstruirActivity.java
+++ app/src/main/java/com/construguia/tecnoparque/construguia/ConstruirActivity.java
@@ -3,13 +3,35 @@ package com.construguia.tecnoparque.construguia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ListView;
+
+import com.construguia.tecnoparque.construguia.DTO.itemLista;
 
 public class ConstruirActivity extends AppCompatActivity {
+
+    ListView listItemConstruir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_construir);
+
+        itemLista[] datos =
+                new itemLista[]{
+                        new itemLista("Piso", "Todo tipo de pisos", R.drawable.piso),
+                        new itemLista("Pared", "Todo tipo de paredes", R.drawable.pared),
+                        new itemLista("Techo", "Todo tipo de techos", R.drawable.pared),
+                        new itemLista("Puerta", "Todo tipo de puertas", R.drawable.piso),
+                        new itemLista("Ventana", "Todo tipo de ventanas", R.drawable.pared),
+                        new itemLista("Luminaria", "Todo tipo de luminarias", R.drawable.piso),
+                };
+
+        adaptador_listview adaptador =
+                new adaptador_listview(this, R.id.listViewConstruir,datos);
+
+        listItemConstruir = (ListView)findViewById(R.id.listViewConstruir);
+
+        listItemConstruir.setAdapter(adaptador);
 
     }
 }
---
+++ app/src/main/java/com/construguia/tecnoparque/construguia/adaptador_listview.java
@@ -0,0 +1,44 @@
+package com.construguia.tecnoparque.construguia;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.construguia.tecnoparque.construguia.DTO.itemLista;
+
+/**
+ * Created by YOLIMA on 26/04/2016.
+ */
+public class adaptador_listview extends ArrayAdapter<itemLista> {
+
+    itemLista[] datos;
+    Context cnt;
+    int layout;
+    public adaptador_listview(Context context, int resource, itemLista[] objects) {
+        super(context, resource, objects);
+        datos=objects;
+        cnt=context;
+        layout=resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item = inflater.inflate(layout, null);
+
+        TextView lblTitulo = (TextView)item.findViewById(R.id.textTitulo);
+        lblTitulo.setText(datos[position].getTitulo());
+
+        TextView lblSubtitulo = (TextView)item.findViewById(R.id.textSubTitulo);
+        lblSubtitulo.setText(datos[position].getSubtitulo());
+
+        return(item);
+
+    }
+}
+
+
